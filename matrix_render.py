71330225a47b3819bae0caf4e16179b4f7d674d3
Anthooony <38412203+Anthooony@users.noreply.github.com>
2024-01-30 23:36:06 +0100

    Update matrix_render.py
    cleaned up code
    added zooming and moving the camera
    random matrix generation for testing

--- matrix_render.py
+++ matrix_render.py
@@ -1,43 +1,57 @@
 import pygame
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
-running = True
 
-t_m = [
-    [1, 0, 1],
-    [0, 1, 0],
-    [0, 0, 1]
-    ]
-t_m2 = [
-    [1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 1, 0, 1, 1, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0, 1, 1, 1],
-    [1, 1, 0, 0, 0, 0, 1, 0, 0],
-    [1, 1, 1, 0, 1, 0, 1, 1, 1],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 0, 1, 0, 1, 1],
-    [1, 1, 1, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 0, 0, 0]
-    ]
+running = True
+dragging = False
+zoom = 1
+cam=[0, 0]
 
 while running==True:
-    cellSize = 800/len(t_m2)
 
+    screen.fill("black")
+
+    matrix=[[random.choice([".", "O"]) for i in range(24)] for h in range(24)]      #the update method will have to be in the pygame loop
     
+    cellSize = int((screen.get_size()[0]/len(matrix))*zoom)
+    
+    if dragging:
+        relx, rely = pygame.mouse.get_rel()
+        cam[0] += relx
+        cam[1] += rely
 
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
-    
-    for y in range(len(t_m2)):
-        for x in range(len(t_m2[y])):
-            if t_m2[x][y]==0:
-                pygame.draw.rect(screen, "black", [y*cellSize, x*cellSize, cellSize, cellSize])
-            else:
-                pygame.draw.rect(screen, "white", [y*cellSize, x*cellSize, cellSize, cellSize])
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if event.button == 4 or event.button == 5:
+                zoom *= 1.25 if event.button == 4 else 0.75
+
+            if event.button == 1:
+                if pygame.mouse.get_focused():
+                    pygame.mouse.get_rel()
+                    dragging = True
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                dragging = False
+
+
+    
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if matrix[x][y] == ".":
+                pygame.draw.rect(screen, "black", [y*cellSize+cam[0], x*cellSize+cam[1], cellSize, cellSize]) #look into why x and y are reversed
+            else:
+                pygame.draw.rect(screen, "white", [y*cellSize+cam[0], x*cellSize+cam[1], cellSize, cellSize])
+
+    
     pygame.display.flip()
     clock.tick(60)
     
